960e75cae6c791568e363dfc5015a195f934be1e
Alex Denysenko <oleksandr.denysenko@intelex.com>
2016-08-04 15:26:02 -0400

    update gulp and tsc

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,14 @@ gulp.task('default', function (done) {
       'node_modules/angular2/bundles/angular2-polyfills.js',
       'node_modules/angular2/bundles/http.*.js*',
       'node_modules/angular2/bundles/router.*.js*',
+      'node_modules/core-js/client/shim.min.js',
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/reflect-metadata/Reflect.js',
+      'node_modules/systemjs/dist/system.src.js',
+      'systemjs.config.js',
+      // temp bullshit
       'node_modules/es6-shim/es6-shim.min.js*',
       'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
-      'node_modules/systemjs/dist/*.*',
       'node_modules/rxjs/bundles/Rx.js',
     ]).pipe(gulp.dest('./wwwroot/lib/js'));
 
